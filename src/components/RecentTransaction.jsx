5f9b76d9946a8c2ec79a744de6a48b9f8fa077e7
pegah <pegah.tavakiloyaraki17@gmail.com>
2023-12-03 00:47:25 -0800

    add comments and new icons to recent transactions

--- src/components/RecentTransaction.jsx
+++ src/components/RecentTransaction.jsx
@@ -34,6 +34,7 @@ export default function RecentTransaction() {
   return (
     <div className="custom-page">
       <h5 className="text-dark-blue mb-3">تراکنش‌های اخیر</h5>
+      {/* loading */}
       {loading && (
         <PulseLoader
           color="#180883"
@@ -41,6 +42,7 @@ export default function RecentTransaction() {
           style={{ height: "80vh" }}
         />
       )}
+      {/* list of recent transactions */}
       {!loading && transactions.length != 0 && (
         <div>
           {transactions.map((item, index) => {
@@ -51,6 +53,19 @@ export default function RecentTransaction() {
                   style={{ backgroundColor: "#180883" }}
                 />
                 <div className="d-flex w-100 p-2">
+                  <span>
+                    {item.type == "income" ? (
+                      <i
+                        className="bi bi-plus-circle-dotted text-success"
+                        style={{ fontSize: "18px" }}
+                      ></i>
+                    ) : (
+                      <i
+                        className="bi bi-dash-circle-dotted text-danger"
+                        style={{ fontSize: "18px" }}
+                      ></i>
+                    )}
+                  </span>
                   <span
                     className={`${
                       item.type == "income" ? "text-success" : "text-danger"
@@ -73,6 +88,7 @@ export default function RecentTransaction() {
           })}
         </div>
       )}
+      {/* button for navigate to new transactions */}
       <NewTransactionBtn />
     </div>
   );
